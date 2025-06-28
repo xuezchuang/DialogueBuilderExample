66ead4954c21a197c80f5cad27cc2136603e6411
xuezc <13436472835@163.com>
2025-06-28 19:07:56 +0800

    Add Source

---
+++ Source/DialogueExample/DialogueExampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DialogueExampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DIALOGUEEXAMPLE_API ADialogueExampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
